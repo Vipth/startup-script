f5ccbcf8328d3fbf3a5b22d50cfca2013e1ea429
Vipth <bradycurtis9@yahoo.com>
2025-03-16 13:10:27 -0500

    update

--- startup.sh
+++ startup.sh
@@ -6,8 +6,8 @@ sudo apt update && sudo apt upgrade
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 # Add Docker's official GPG key:
-sudo apt-get update 
-sudo apt-get install ca-certificates curl
+sudo apt-get update
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc 
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -20,7 +20,7 @@ echo \
 
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 ## Enable SSH
 sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
